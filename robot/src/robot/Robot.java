52c22cc6f4c111a03c6d3c38f624d5a73f01c87e
uheai <maikwis@gmail.com>
2017-01-30 16:01:24 +0100

    added listener for ir sensor

--- robot/src/robot/Robot.java
+++ robot/src/robot/Robot.java
@@ -1,5 +1,7 @@
 package robot;
 
+import java.beans.FeatureDescriptor;
+
 import lejos.hardware.motor.Motor;
 import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
@@ -7,8 +9,13 @@ import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3GyroSensor;
 import lejos.hardware.sensor.EV3IRSensor;
 import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.robotics.RangeFinderAdapter;
 import lejos.robotics.RegulatedMotor;
 import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.objectdetection.Feature;
+import lejos.robotics.objectdetection.FeatureDetector;
+import lejos.robotics.objectdetection.FeatureListener;
+import lejos.robotics.objectdetection.RangeFeatureDetector;
 
 /**
  * class representing robot
@@ -36,7 +43,7 @@ public class Robot {
 	//Sensors
 	
 	private Port colorSensorPort = SensorPort.S1; //TODO
-	private Port irSensorPort = SensorPort.S1; //TODO
+	private Port irSensorPort = SensorPort.S4; //TODO
 	private Port gyroSensorPort = SensorPort.S1; //TODO
 	private Port touchSensor1Port = SensorPort.S1; //TODO
 	private Port touchSensor2Port = SensorPort.S1; //TODO
@@ -51,7 +58,7 @@ public class Robot {
 		//TODO: set variables
 		pilot = new DifferentialPilot(wheelDiameter, trackWidth, leftWheel, rightWheel);
 //		colorSensor = new EV3ColorSensor(colorSensorPort);
-//		irSensor = new EV3IRSensor(irSensorPort);
+		irSensor = new EV3IRSensor(irSensorPort);
 //		gyroSensor = new EV3GyroSensor(gyroSensorPort);
 //		touch1 = new EV3TouchSensor(touchSensor1Port);
 //		touch2 = new EV3TouchSensor(touchSensor2Port);
@@ -62,6 +69,15 @@ public class Robot {
 	 * starts the robot
 	 */
 	public void start() {
- 		pilot.travel(30);
+ 		//pilot.forward();
+ 		FeatureDetector detector = new RangeFeatureDetector(new RangeFinderAdapter(irSensor.getDistanceMode()), 40, 5000);
+ 		detector.addListener(new FeatureListener() {
+			
+			@Override
+			public void featureDetected(Feature feature, FeatureDetector detector) {
+				System.out.println(feature.getRangeReading().getRange());
+			}
+		});
+ 		
 	}
 }
