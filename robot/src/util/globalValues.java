b9d351c0987c97c2815a367a507000ab76248e7c
BENNY-LAPTOP\Benny <benjamindornfeld@gmx.de>
2017-01-31 16:14:17 +0100

    Changed MINLIGHT and MAXLIGHT for debugging

--- robot/src/util/globalValues.java
+++ robot/src/util/globalValues.java
@@ -5,7 +5,7 @@ public final class globalValues {
 	public static final float MINLIGHT = 0.05f;		//real Values 
 	public static final float MAXLIGHT = 0.4f;
 	*/
-	public static final float MINLIGHT = 0.05f;		//Test Values (Floor) 
-	public static final float MAXLIGHT = 0.07f;
+	public static final float MINLIGHT = 0.1f;		//Test Values (Floor) 
+	public static final float MAXLIGHT = 0.2f;
 	
 }
