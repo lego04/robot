e968dc482bdb1a26d6ec40b8f0a5a678c63fa3b9
uheai <maikwis@gmail.com>
2017-01-31 12:20:37 +0100

    removed LightSensorListener, added function to get light value

--- robot/src/robot/LineFollower.java
+++ robot/src/robot/LineFollower.java
@@ -4,16 +4,14 @@ import lejos.hardware.Button;
 import lejos.hardware.Key;
 import lejos.hardware.KeyListener;
 import lejos.robotics.LightDetectorAdaptor;
-import lejos.utility.DebugMessages;
-import listeners.LightSensorListener;
 
 public class LineFollower {
-	
+
 	/**
 	 * pointer to robot instance
 	 */
 	private Robot robot;
-	
+
 	/**
 	 * interface for color / light sensor
 	 */
@@ -25,45 +23,44 @@ public class LineFollower {
 		detector.setLow(0);
 		detector.setHigh(100);
 		detector.setReflected(true);
-		
+
 	}
-	
-	
-	
+
 	public void followLine() {
-		
+
 	}
-	
-	private void startLightSensorThread() {
-		Thread lightSensorThread = new Thread(new LightSensorListener(this));
-		lightSensorThread.start();
-	}
-	
+
 	public void update() {
-		
+
 	}
 	
+	private float getCurrentNormalizedLightValue() {
+		float lv = detector.getNormalizedLightValue();
+		System.out.println("norm light value: " + lv);
+		return lv;
+	}
+
 	/**
 	 * shows current light value whenever enter is pressed
 	 */
 	public void debug() {
-Button.ENTER.addKeyListener(new KeyListener() {
-			
-			//private DebugMessages debug = new DebugMessages();
-			
+		Button.ENTER.addKeyListener(new KeyListener() {
+
+			// private DebugMessages debug = new DebugMessages();
+
 			@Override
 			public void keyReleased(Key k) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void keyPressed(Key k) {
 				if (k.getId() == Button.ID_DOWN) {
 					float light = detector.getNormalizedLightValue();
 					System.out.println(light);
 				}
-				
+
 			}
 		});
 	}
