b6890eccdc69617a553b25923f513865ef79bc38
Rashad Asgarbayli <rashad2985@gmail.com>
2017-01-31 15:53:15 +0100

    Start with the implementation of the controller, that tries to keep the robot at the wall.

--- robot/src/robot/WallFollower.java
+++ robot/src/robot/WallFollower.java
@@ -12,14 +12,46 @@ public abstract class WallFollower {
 	private DifferentialPilot pilot;
 	private UltrasonicSensor sensor;
 	private float distanceToWall;
-	private float acceptableDistance;
+	private final float acceptableDistance;
 	
 	public WallFollower(Robot robot, UltrasonicSensor sensor) {
 		this.pilot = robot.getPilot();
 		this.sensor = sensor;
-		this.distanceToWall = sensor.getLeftDistance();
+		this.distanceToWall = 0.0f;
+		updateDistanceToWall();
 		this.acceptableDistance = 0.0f;
 	}
 	
+	/**
+	 * Robot follows the wall using it as an anchor point to find its way through the labyrinth.
+	 * Robot stays in this state, until it decides, that it is out of the labyrinth.
+	 */
+	public void followTheWall() {
+		while (isInLabyrinth()) {
+			controllTheDistanceToWall();
+		}
+	}
 	
+	/**
+	 * Decides, if the robot still in the labyrinth or not.
+	 * @return <b>true</b>, if the robot still in the labyrinth, else <b>false</b>.
+	 */
+	private boolean isInLabyrinth() {
+		// TODO: Decide to change state, if the robot out of the maze.
+		return true;
+	}
+	
+	/**
+	 * Controller, that tries to keep the robot at the wall.
+	 */
+	private void controllTheDistanceToWall() {
+		// TODO: Regler to follow the wall and at the same time keep the distance acceptable between them.
+	}
+	
+	/**
+	 * Updates the distance between the wall and the 
+	 */
+	private void updateDistanceToWall() {
+		this.distanceToWall = sensor.getLeftDistance();
+	}
 }
