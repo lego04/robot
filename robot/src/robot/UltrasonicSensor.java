e56d93670845c8a1b09b4d6c96cbe6a2d9af0385
tes <thomas.erik.stoehr@gmail.com>
2017-02-01 11:53:07 +0100

    usSensorTest

--- robot/src/robot/UltrasonicSensor.java
+++ robot/src/robot/UltrasonicSensor.java
@@ -1,53 +1,93 @@
 package robot;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import lejos.robotics.RangeFinderAdapter;
 import lejos.robotics.RegulatedMotor;
 import lejos.robotics.SampleProvider;
+import util.globalValues;
 
-public class UltrasonicSensor {
+public class UltrasonicSensor implements Runnable{
 
+	public enum Modes {
+		Labyrinth,
+		Bridge
+	}
+	
 	private Robot robot;
-	//private RotatingRangeScanner rotScanner;
+	private Modes mode;
+	
 	private RangeFinderAdapter rangeFinder;
 	private RegulatedMotor usMotor;
 	
-// TODO: richtige Werte
-	private float leftDistance = 0;
-	private float rightDistance = 0;
+	private AtomicBoolean active;
+	private Thread usSensorThread;
+	
+	private AtomicInteger leftDistance;
+	private AtomicInteger rightDistance;
 	
 	public UltrasonicSensor(Robot robot) {
 		
-		this.robot = robot;		
+		this.robot = robot;	
+// TODO: Set real starting mode
+		mode = Modes.Labyrinth;
 		
 		SampleProvider sampleProvider = robot.getUSSensor().getDistanceMode();
 		rangeFinder = new RangeFinderAdapter(sampleProvider);
 		
 		usMotor = robot.ultrasonicMotor;
 		
+		leftDistance = new AtomicInteger();
+		rightDistance = new AtomicInteger();
+		
+		leftDistance.set(0);
+		rightDistance.set(0);
+		
 	}
 	
 	public float getLeftDistance() {
-		return leftDistance; // in Metern
+		return ((float) leftDistance.get()) / globalValues.floatToInt; // in Metern?
 	}
 	
 	public float getRightDistance() {
-		return rightDistance; // in Metern
+		return ((float) rightDistance.get()) / globalValues.floatToInt; // in Metern?
 	}
 	
 	public void start() {
 		
+		startWalldetection();
+	}
+	
+	private void startWalldetection() {
+		usMotor.rotateTo(0);
 		usMotor.rotate(90);
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 100000; i++) {
 			if (i % 2 == 0) {
 				usMotor.rotate(-180);
-				rightDistance = rangeFinder.getRange();
+				int rightDistanceInt = (int) (rangeFinder.getRange() * globalValues.floatToInt);
+				rightDistance.set(rightDistanceInt);
 				System.out.println("right: " + rightDistance);
 			} else {
 				usMotor.rotate(180);
-				leftDistance = rangeFinder.getRange();
+				int leftDistanceInt = (int) (rangeFinder.getRange() * globalValues.floatToInt);
+				leftDistance.set(leftDistanceInt);
 				System.out.println("left: " + leftDistance);
 			}
 		}
+	}
+	
+	private void startBridgeDetection() {
+		usMotor.rotateTo(0);
+// TODO: implement
+	}
+
+	@Override
+	public void run() {
+		if (mode == Modes.Labyrinth) {
+			
+		}
+		
 	}
 
 }
--- robot/src/test/USSensorTest.java
+++ robot/src/test/USSensorTest.java
@@ -4,13 +4,15 @@ import lejos.hardware.Button;
 import lejos.hardware.Key;
 import robot.Robot;
 import robot.UltrasonicSensor;
+import util.EscapeThread;
 
 public class USSensorTest {
 	
 	public static void main(String[] args) {
+		new EscapeThread().startThread();
 		// TODO Auto-generated method stub
 		Robot robot = new Robot();
-		robot.start();
+//		robot.start();
 		//System.out.println("Hello World");
 		
 		UltrasonicSensor us = new UltrasonicSensor(robot);
