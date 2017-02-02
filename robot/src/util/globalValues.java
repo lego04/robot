8fec62682bdd06097de84e5ee3692dfd1fd91b94
BENNY-LAPTOP\Benny <benjamindornfeld@gmx.de>
2017-02-02 11:59:38 +0100

    Tweaked some Values for the LineFollower (yet to be tested)

--- robot/src/util/globalValues.java
+++ robot/src/util/globalValues.java
@@ -5,8 +5,8 @@ public final class globalValues {
 	public static final float MINLIGHT = 0.05f;		//real Values 
 	public static final float MAXLIGHT = 0.4f;
 	*/
-	public static final float MINLIGHT = 0.05f;		//Test Values (Floor) 
-	public static final float MAXLIGHT = 0.3f;
+	public static final float MINLIGHT = 0.04f;		//Test Values (Floor) 
+	public static final float MAXLIGHT = 0.35f;
 	public static final float LEFT = 1f;
 	public static final float RIGHT = -1f;
 	
