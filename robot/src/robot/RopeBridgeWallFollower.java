4d4b79bf6d05bd3e3825b4b5340a0440cced7707
tes <thomas.erik.stoehr@gmail.com>
2017-02-07 18:27:16 +0100

    Different approach for RopeBridgeFollower

--- robot/src/robot/RopeBridgeWallFollower.java
+++ robot/src/robot/RopeBridgeWallFollower.java
@@ -1,24 +1,25 @@
 package robot;
 
-//import sensorThreads.GyroSensorThread;
-import util.GlobalValues;
-
 public class RopeBridgeWallFollower extends WallFollower2 {
 	
 	private boolean active;
 	
 	private int MIN_DIST = 80;
 	private int MAX_DIST = 130;
-	private int INF_DIST = 500;
+	private int INF_DIST = 250;
 	private int CRITICAL_DIST = 50;
 	private int SPEED = 150;
 	
-//	private GyroSensorThread gyro;
+	private int IDEAL_DIST = 80;
+	
+	private int infCounter;
 	
 	public RopeBridgeWallFollower(Robot robot) {
 		super(robot);
 		active = true;
-		//gyro = robot.getThreadPool().getGyroSensorThread();
+		robot.getRightWheel().setSpeed(SPEED);
+		robot.getLeftWheel().setSpeed(SPEED);
+		infCounter = 0;
 	}
 	
 	@Override
@@ -26,6 +27,7 @@ public class RopeBridgeWallFollower extends WallFollower2 {
 		while (active) {
 			stayOnWall();
 		}
+		/*
 		robot.getMovement().stopAll();
 		robot.getMovement().turnOnPointLeft(10);
 		robot.getLeftWheel().setSpeed(SPEED);
@@ -39,11 +41,45 @@ public class RopeBridgeWallFollower extends WallFollower2 {
 		}
 		//robot.getMovement().stopAll();
 		distanceSensor.rotateSensor(90);
+		*/
 	}
 	
 	@Override
-	public void stayOnWall() {
+	public void stayOnWall() {		
 		updateDistanceToWall();
+		System.out.println(isDistance);
+		int diff = IDEAL_DIST - isDistance;
+		//System.out.println(diff);
+		int minusSpeed = (int) SPEED - (diff / 2) ;
+		int plusSpeed = (int) SPEED + (diff / 2);
+		//System.out.println(SPEED + "+" + diff + "=" + speed );
+		if (minusSpeed > 2 * SPEED) {
+			minusSpeed = 2 * SPEED;
+		}
+		if (plusSpeed > 2 * SPEED) {
+			plusSpeed = 2 * SPEED;
+		}
+		robot.getLeftWheel().setSpeed(minusSpeed);
+		
+		if (isDistance > INF_DIST) {
+			infCounter++;
+			if (infCounter >= 10) {
+				goForwardAndHopeForTheBest();
+			}
+		} else {
+			infCounter = 0;
+			robot.getRightWheel().setSpeed(plusSpeed);
+			robot.getLeftWheel().setSpeed(minusSpeed);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
 		if (isDistance < MIN_DIST) {
 			if (isDistance < CRITICAL_DIST) {
 				robot.getLeftWheel().setSpeed(25);
@@ -71,7 +107,27 @@ public class RopeBridgeWallFollower extends WallFollower2 {
 			robot.getLeftWheel().setSpeed(SPEED);
 			robot.getRightWheel().setSpeed(SPEED);
 		}
+		*/
 		robot.getMovement().goForward();
+		
+	}
+	
+	private void goForwardAndHopeForTheBest() {
+		System.out.println("FORWARD");
+		robot.getMovement().turnOnPointRight(10);
+		robot.getLeftWheel().setSpeed(3 * SPEED);
+		robot.getRightWheel().setSpeed(3 * SPEED);
+		robot.getMovement().goForward();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+/*		while (true) {
+// TODO: implement
+			// 	if (nächstes Hindernis) { return } // und aus aeusserem Loop raus
+		}*/
 	}
 	
 }
