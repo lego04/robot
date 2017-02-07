60a763d6c637caa84d5e4b87afea0a3987e1bb2a
BENNY-LAPTOP\Benny <benjamindornfeld@gmx.de>
2017-02-07 17:46:13 +0100

    Adjusted the FindLineFirst Class

--- robot/src/robot/FindLineFirst.java
+++ robot/src/robot/FindLineFirst.java
@@ -24,6 +24,7 @@ public class FindLineFirst {
 		robot.getMovement().goForward();
 		while (lst.getLastLightValue() < GlobalValues.AVG_LIGHT) {
 		}
+		robot.getMovement().turnOnPointRight(30);
 		robot.getMovement().turnOnPointRight();
 		while (lst.getLastLightValue() < GlobalValues.MAXLIGHT) {
 		}
@@ -32,12 +33,26 @@ public class FindLineFirst {
 	}
 	
 	public void findLineAfterBridge() {
+		robot.getMovement().forwardDirection();
 		robot.getMovement().setSpeed(GlobalValues.LINETRAVELSPEED);
 		robot.getMovement().turnOnPointRight(110);
 		robot.getMovement().forwardDirection();
 		robot.getMovement().goForward();
 		while (lst.getLastLightValue() < GlobalValues.AVG_LIGHT) { }
 		robot.getMovement().turnOnPointLeft(60);
+		new LineFollower(robot).adjustLine();
+	}
+	
+	public void findStraightLine() {
+		robot.getMovement().setSpeed(GlobalValues.LINETRAVELSPEED);
+		robot.getMovement().forwardDirection();
+		robot.getMovement().goForwardDist(10);
+		if (lst.getLastLightValue() > GlobalValues.AVG_LIGHT) {
+			new LineFollower(robot).adjustLine();
+		}
+		robot.getMovement().turnOnPointLeft();
+		while (lst.getLastLightValue() > GlobalValues.AVG_LIGHT) {
+		}
 		new LineFollower(robot).adjustLine();
 	}
 	
