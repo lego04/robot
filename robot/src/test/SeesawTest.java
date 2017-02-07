3ff94f302912524a07c5b5b8f7696057b6e87a6b
BENNY-LAPTOP\Benny <benjamindornfeld@gmx.de>
2017-02-07 17:47:00 +0100

    Additions to the SeesawTest

--- robot/src/test/SeesawTest.java
+++ robot/src/test/SeesawTest.java
@@ -13,6 +13,7 @@ public class SeesawTest {
 		LightSensorThread lst = new LightSensorThread(robot);
 		lst.startThread();
 		
+		new LineFollower(robot).adjustLine();
 		new LineFollower(robot).adjustLine(true);
 		robot.getMovement().setSpeed(GlobalValues.LINETRAVELSPEED / 2);
 		robot.getMovement().goForwardDist(10);
