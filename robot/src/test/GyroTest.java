93d047976dde4b42257d72f3c9769e426da1415a
BENNY-LAPTOP\Benny <benjamindornfeld@gmx.de>
2017-02-08 09:21:33 +0100

    Adjusted Gyro Test

--- robot/src/test/GyroTest.java
+++ robot/src/test/GyroTest.java
@@ -2,7 +2,10 @@ package test;
 
 import robot.Robot;
 import sensorThreads.GyroSensorThread;
+import sensorThreads.UltrasonicSensorThread;
+import sensorThreads.UltrasonicSensorThread.Modes;
 import util.EscapeThread;
+import util.GlobalValues;
 
 public class GyroTest {
 	
@@ -12,22 +15,30 @@ public class GyroTest {
 		Robot robot = new Robot();
 		
 		GyroSensorThread gst = new GyroSensorThread(robot);
+		UltrasonicSensorThread ust = new UltrasonicSensorThread(robot);
+		ust.start(Modes.Left);
 		gst.start();
 		
 		int i = 0;
 		
 		while (true) {
+			
 			System.out.println("Gyro: " + gst.getAngle());
-			try {
-				Thread.sleep(500);
-			}
-			catch (Exception e) { }
-			i++;
-			if (i > 20) {
-				gst.reset();
-				System.out.println("Resetting");
-				i = 0;
-			}
+			System.out.println("Dist: " + ust.getDistance());
+			robot.getMovement().setSpeed(GlobalValues.WALLFOLLOWSPEED);
+			robot.getMovement().forwardDirection();
+			robot.getMovement().goForwardDist(25);
+			System.out.println("Dist: " + ust.getDistance());
+//			try {
+//				Thread.sleep(500);
+//			}
+//			catch (Exception e) { }
+//			i++;
+//			if (i > 20) {
+//				gst.reset();
+//				System.out.println("Resetting");
+//				i = 0;
+//			}
 		}
 		
 	}
